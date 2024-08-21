e9f693814f3ac3dff3630c25fcaa067f37b29678
ZinZin167 <tranvinhk13@gmail.com>
2024-08-21 08:31:25 +0700

    Add files via upload

---
+++ giaiptb1.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+	float a, b;
+	printf("\nNhap he so a: ");
+	scanf_s("%f", &a);
+	printf("\nNhap he so b: ");
+	scanf_s("%f", &b);
+	if (a == 0){      //nếu a==0 thì
+		if (b == 0){    //nếu b==0 thì
+			printf("\nPhuong trinh vo so nghiem");
+		}
+		else{       //nếu không thì (nếu b!=0 thì)
+			printf("\nPhuong trinh vo nghiem");
+		}
+	}
+	else{         //nếu không thì (nếu a!=0 thì)
+		printf("\nPhuong trinh co nghiem x=%g", -b / a);
+	}
+	_getch();
+}
